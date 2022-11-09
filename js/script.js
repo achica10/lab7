d43c5d58e4f39b6fbf5df0321c7f23856532657d
achica10 <112531727+achica10@users.noreply.github.com>
2022-11-08 21:49:18 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 function init(){
   function alertBox(){
     var textInput = document.getElementById('entryinput');
+	alert("Alexandru Chica: " + textInput.value);
+	
 	var textOutput = document.getElementById('textoutput');
-	textOutput.innerHTML = textInput.value;
-	alert("Alexandru Chica: " + textInput);
+	textOutput.innerHTML = textInput.value;	
   }
- 
-
-
-
-
-
-
-
+  var alertButton = document.getElementById('entrybutton');
+  alertButton.addEventListener('click', alertBox);
+  
+}
 window.addEventListener('load', init);
